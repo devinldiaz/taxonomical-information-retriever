53e0a40652d6a9a70ae842c79b9a7477844be7f7
Devin <devinlainediaz@gmail.com>
2025-11-29 21:01:10 -0500

    create reusable cards

--- digenea.py
+++ digenea.py
@@ -1,38 +1,16 @@
 import streamlit as st
-from streamlit_extras.let_it_rain import rain
-import main
+from utils import parasite_card
+
 st.title("Digenea")
 
-# create many containers for several cards, each with heading, a search button, photo, and text. and the button will return relevant info
 row1 = st.columns(3)
 row2 = st.columns(3)
 row3 = st.columns(3)
-# for col in row:
-#     
-#     tile = col.container(height = 300)
 
 
 with row1[0]:
-    # st.html("<p style='background-color: red'>it's working</p>");
-    tile = row1[0].container(height = 300)
-    st.header("Schistosoma mansoni")
-
-    with tile:
-        st.image("https://www.ncbi.nlm.nih.gov/Taxonomy/taxi/images/4049", width=150)
-        st.write("This is an example card with an image and some text.")
-        if st.button("S. mansoni taxonomy information"):
-            info = main.get_info("Schistosoma mansoni")
-            st.header("NCBI Taxonomical Information")
-            if isinstance(info, str):
-                st.error(info)
-            else:
-                st.balloons()
-                st.success(f"**Name Found:** *{info['Scientific Name']}* ({info['Rank'].capitalize()})")
-                st.markdown(f"**Taxonomy ID:** `{info['Taxonomy ID']}`")
-                st.markdown(f"**Full Lineage:** {info['Lineage']}")
-                st.markdown('---')
-            
-
+    st.write(parasite_card("https://www.xpedite-dx.com/fileadmin/benutzerdaten/xpedite-dx-com/bilder/Applications/Microscopy_Schistosoma_mansoni_adult.jpg", "Schistosoma mansoni", "A parasitic worm that causes schistosomiasis in humans, primarily affecting the intestines and liver."))
+        
 with row1[1]:
     tile = row1[1].container(height = 300)
     st.header("Schistosoma haematobium")
--- main.py
+++ main.py
@@ -1,45 +1,5 @@
-import re
 import streamlit as st
-from Bio import Entrez
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-Entrez.email = os.getenv("NCBI_EMAIL")
-
-def get_info(name):
-    try:
-        handle = Entrez.esearch(db="taxonomy", term=name)
-        record = Entrez.read(handle)
-        handle.close()
-
-        if not record["IdList"]:
-            return "No match found in NCBI Taxonomy."
-        
-        tax_id = record["IdList"][0]
-        handle = Entrez.efetch(db="taxonomy", id=tax_id, retmode="xml")
-        tax_record = Entrez.read(handle)
-        handle.close()
-
-        scientific_name = tax_record[0]["ScientificName"]
-        rank = tax_record[0]["Rank"]
-        lineage = tax_record[0]["LineageEx"]
-
-        full_lineage = " > ".join([item['ScientificName'] for item in lineage])
-
-        return{
-            "Scientific Name": scientific_name,
-            "Rank": rank,
-            "Taxonomy ID": tax_id,
-            "Lineage": full_lineage
-        }
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        return None
-    
-
-# Define the pages
 main_page = st.Page("home.py", title="Home", icon="🏠")
 page_2 = st.Page("digenea.py", title="Digenea", icon="✨")
 page_3 = st.Page("cestoda.py", title="Cestoda", icon="✨")
