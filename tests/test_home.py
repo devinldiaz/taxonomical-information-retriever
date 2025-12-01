7ef10d945478363c66b1c503a60949f6358d094d
Devin <devinlainediaz@gmail.com>
2025-11-30 21:04:28 -0500

    edit workflow

--- tests/test_home.py
+++ tests/test_home.py
@@ -2,12 +2,16 @@ from streamlit.testing.v1 import AppTest
 
 at = AppTest.from_file("home.py").run()
 
+
 def test_home_page_title():
     assert len(at.title) > 0
     assert "Taxonomical Information Retriever" in at.title[0].value
 
+
 def test_input():
-    assert "Enter the scientific name of an organism:" in at.text_input[0].label
+    search_text = "Enter the scientific name of an organism:"
+    assert search_text in at.text_input[0].label
+
 
 def test_search_button():
     assert "Search" in at.button[0].label
