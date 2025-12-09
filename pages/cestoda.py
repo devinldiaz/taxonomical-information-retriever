051398718ed2186b9ab54d954f1c915765f7b30b
Devin <devinlainediaz@gmail.com>
2025-12-09 14:55:52 -0500

    parasite pages

--- pages/cestoda.py
+++ pages/cestoda.py
@@ -1,3 +1,10 @@
 import streamlit as st
+from utils import parasite_card, get_info, plot_phylogeny
 
 st.title("Cestoda")
+
+plot = plot_phylogeny(
+    get_info("Taenia solium")["Lineage"]
+)
+
+st.pyplot(plot)
