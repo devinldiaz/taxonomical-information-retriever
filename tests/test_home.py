73e7312ee7aaa17c8b1cb61a1fd714ba0be8126a
Devin <devinlainediaz@gmail.com>
2025-12-16 11:09:22 -0500

    fix home page tests

--- tests/test_home.py
+++ tests/test_home.py
@@ -5,12 +5,12 @@ at = AppTest.from_file("pages/home.py").run()
 
 def test_home_page_title():
     assert len(at.title) > 0
-    assert "Taxonomical Information Retriever" in at.title[0].value
+    assert "ParaSite" in at.title[0].value
 
 
 def test_input():
-    search_text = "Enter the scientific name of an organism:"
-    assert search_text in at.text_input[0].label
+    search = "Parasite not listed? Enter the scientific name & search here:"
+    assert search in at.text_input[0].label
 
 
 def test_search_button():
